8ef65cbb5e8e1c4bf193f8455065214c959fa8df
Dmitro Yaremchuk <145034908+bobmarley2301@users.noreply.github.com>
2025-01-10 09:56:03 +0200

    add JS

--- main.js
+++ main.js
@@ -7,19 +7,10 @@ const line2 = document.getElementById("line-2");
 const line3 = document.getElementById("line-3");
 
 // Check if elements exist to avoid errors
-if (
-  navbar &&
-  navLinks &&
-  burgerMenu &&
-  line1 &&
-  line2 &&
-  line3
-) {
-  // Add event listener for scroll
-  window.addEventListener("scroll", function () {
-    // Check if menu is open
+if (navbar && navLinks && burgerMenu && line1 && line2 && line3) {
+  // Function to handle navbar background color
+  const updateNavbarBackground = () => {
     const isMenuOpen = !navLinks.classList.contains("hidden");
-
     if (window.scrollY > 50 || isMenuOpen) {
       navbar.classList.add("bg-[#3C6255]");
       navbar.classList.remove("bg-transparent");
@@ -29,31 +20,34 @@ if (
       navbar.classList.remove("bg-[#3C6255]");
       navLinks.classList.remove("bg-[#3C6255]");
     }
-  });
+  };
+
+  // Add event listener for scroll
+  window.addEventListener("scroll", updateNavbarBackground);
 
   // Burger menu click event
   burgerMenu.addEventListener("click", () => {
-    // Toggle menu visibility
-    if (navLinks.classList.contains("hidden")) {
+    const isHidden = navLinks.classList.contains("hidden");
+
+    // Toggle visibility of navLinks
+    if (isHidden) {
       navLinks.classList.remove("hidden");
       navLinks.style.maxHeight = `${navLinks.scrollHeight}px`; // Animate opening
       setTimeout(() => {
         navLinks.style.maxHeight = "none"; // After animation, set max height to auto
       }, 500);
-
-      // Ensure background color is applied when menu opens
       navbar.classList.add("bg-[#3C6255]");
       navLinks.classList.add("bg-[#3C6255]");
     } else {
       navLinks.style.maxHeight = "0"; // Animate closing
       setTimeout(() => {
         navLinks.classList.add("hidden");
-      }, 500); // Match the duration of animation
+      }, 500);
 
       // Remove background color if scroll position is at the top
       if (window.scrollY <= 50) {
-        navbar.classList.add("bg-transparent");
         navbar.classList.remove("bg-[#3C6255]");
+        navbar.classList.add("bg-transparent");
         navLinks.classList.remove("bg-[#3C6255]");
       }
     }
